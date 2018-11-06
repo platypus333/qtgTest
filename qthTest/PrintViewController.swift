2c26292b18cb26d822a133e9ca2e5f5a0654ba31
platypus333 <qtinghao@163.com>
2018-11-06 15:49:22 +0800

    install

---
+++ qthTest/PrintViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PrintViewController.swift
+//  qthTest
+//
+//  Created by EJU on 2018/11/6.
+//  Copyright © 2018年 EJU. All rights reserved.
+//
+
+import UIKit
+
+class PrintViewController: UIViewController {
+    override func viewDidLoad() {
+        super .viewDidLoad()
+        print("viewdidload")
+    }
+    
+    func printt() {
+        print("shuchu")
+    }
+}
---
+++ qthTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  qthTest
+//
+//  Created by EJU on 2018/11/6.
+//  Copyright © 2018年 EJU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
